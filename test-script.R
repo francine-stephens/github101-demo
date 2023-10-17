28889732f1150bfb5aba655c422347d44778f1b4
Francine Stephens <fis@stanford.edu>
2023-10-17 13:14:46 -0500

    add config settings

--- test-script.R
+++ test-script.R
@@ -1,3 +1,14 @@
+## install if needed (do this exactly once):
+## install.packages("usethis")
+
+library(usethis)
+use_git_config(user.name = "Francine Stephens", user.email = "fis@stanford.edu")
+
+
+
+
+
+
 # Test Script
 
 print(10 + 5)
